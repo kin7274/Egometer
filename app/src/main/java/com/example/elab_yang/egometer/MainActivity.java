63f405658bf6c11221ab2e2d8a2e94f4c6698dc7
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-11 22:39:10 +0900

    장치 추가 버튼 구현
    장치 추가 버튼 구현

--- app/src/main/java/com/example/elab_yang/egometer/MainActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.elab_yang.egometer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setToolbar();
         setStatusbar();
+        set();
     }
 
     // 툴바
@@ -29,5 +33,23 @@ public class MainActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryPurle));
+    }
+
+    // 객체 생성
+    public void set(){
+        // 장치 추가 버튼
+        Button add_device = (Button) findViewById(R.id.add_device);
+        add_device.setOnClickListener(this);
+    }
+
+    // 클릭 이벤트
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            // 장치 추가하러 ㄱㄱ
+            case R.id.add_device:
+                startActivity(new Intent(MainActivity.this, DeviceChooseActivity.class));
+                break;
+        }
     }
 }
