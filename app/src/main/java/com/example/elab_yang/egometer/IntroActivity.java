fbc537f5fd94a3395f110b021c713ae43569e994
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-14 16:25:48 +0900

    인트로 추가
    인트로 추가

---
+++ app/src/main/java/com/example/elab_yang/egometer/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.elab_yang.egometer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button next_btn = (Button) findViewById(R.id.next_btn);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 시작하기
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
