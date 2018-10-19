ffca68e49f37928020e667b36faec09f524b181b
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-19 21:16:22 +0900

    메인 뷰 수정
    메인 뷰 수정

--- app/src/main/java/com/example/elab_yang/egometer/activity/MainActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/activity/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -37,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //
         Button getDB = (Button) findViewById(R.id.getDB);
         getDB.setOnClickListener(v -> {
