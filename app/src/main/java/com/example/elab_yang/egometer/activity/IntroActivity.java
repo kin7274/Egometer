8be783a5d1f8d1a3974568297c427dff4a84c748
kin7274 <kms6742@naver.com>
2018-10-22 01:51:59 +0900

    인트로 정리
    인트로 정리

--- app/src/main/java/com/example/elab_yang/egometer/activity/IntroActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/activity/IntroActivity.java
@@ -13,12 +13,10 @@ import android.widget.TextView;
 import com.example.elab_yang.egometer.R;
 
 public class IntroActivity extends AppCompatActivity {
-    SharedPreferences pref;
-
     @Override
     protected void onStart() {
         super.onStart();
-        pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
+        SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
         if (pref.getBoolean("activity_executed", false)) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
@@ -31,7 +29,7 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        TextView next_btn = (TextView) findViewById(R.id.next_btn);
+        Button next_btn = (Button) findViewById(R.id.next_btn);
         next_btn.setOnClickListener(v -> {
             // 시작하기
             Intent intent = new Intent(IntroActivity.this, DeviceScanActivity.class);
