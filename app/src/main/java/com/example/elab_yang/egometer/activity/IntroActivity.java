80a542766b137fad8ad36f2babad7a631b82f6d2
kin7274 <kms6742@naver.com>
2018-10-22 01:50:40 +0900

    컬러 변경
    컬러 변경

--- app/src/main/java/com/example/elab_yang/egometer/activity/IntroActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/activity/IntroActivity.java
@@ -14,11 +14,12 @@ import com.example.elab_yang.egometer.R;
 
 public class IntroActivity extends AppCompatActivity {
     SharedPreferences pref;
+
     @Override
     protected void onStart() {
         super.onStart();
         pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
-        if(pref.getBoolean("activity_executed", false)){
+        if (pref.getBoolean("activity_executed", false)) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
@@ -30,7 +31,6 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         TextView next_btn = (TextView) findViewById(R.id.next_btn);
         next_btn.setOnClickListener(v -> {
             // 시작하기
