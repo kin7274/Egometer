5dea453d3c764214762cef81ef9603d81abc9264
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-22 18:52:06 +0900

    네비게이션드로워 추가
    네비게이션드로워 추가

--- app/src/main/java/com/example/elab_yang/egometer/activity/MainActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/activity/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.elab_yang.egometer.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.elab_yang.egometer.R;
 import com.example.elab_yang.egometer.adapter.DeviceAdapter;
@@ -20,7 +28,9 @@ import java.util.HashSet;
 
 import io.paperdb.Paper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     private static final String TAG = "MainActivity";
     private static final long RIPPLE_DURATION = 250;
     RecyclerView recyclerView;
@@ -32,15 +42,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setToolbar();
         setStatusbar();
-        Button getDB = (Button) findViewById(R.id.getDB);
-        getDB.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, getDBActivity.class);
-            startActivity(intent);
-        });
-        Button add_device = (Button) findViewById(R.id.add_device);
-        add_device.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MachineScanActivity.class)));
+
+        Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mytoolbar);
+        getSupportActionBar().setTitle("");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mytoolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+//        Button getDB = (Button) findViewById(R.id.getDB);
+//        getDB.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, getDBActivity.class);
+//            startActivity(intent);
+//        });
+//        Button add_device = (Button) findViewById(R.id.add_device);
+//        add_device.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MachineScanActivity.class)));
         Paper.init(this);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -61,16 +83,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void setToolbar() {
-        Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(mytoolbar);
-        getSupportActionBar().setTitle("");
-    }
-
     public void setStatusbar() {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryPurle));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.nav_add_device:
+                // 장치 추가
+//                Toast.makeText(getApplicationContext(),"장치 추가", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, MachineScanActivity.class));
+                break;
+            case R.id.nav_db_check:
+                // DB 확인
+//                Toast.makeText(getApplicationContext(),"DB 확인", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, getDBActivity.class));
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
