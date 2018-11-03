e98bb021a2780a895991ff2f86cccc67b498565e
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-11-03 18:14:38 +0900

    db 삭제 액티비티 추가
    db 삭제 액티비티 추가

--- app/src/main/java/com/example/elab_yang/egometer/DeleteDataBaseActivity.java
+++ app/src/main/java/com/example/elab_yang/egometer/DeleteDataBaseActivity.java
@@ -1,60 +1,70 @@
 package com.example.elab_yang.egometer;
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.elab_yang.egometer.model.DB;
+import com.example.elab_yang.egometer.model.DB2;
+
 public class DeleteDataBaseActivity extends AppCompatActivity {
-    Context mContext;
     private static final String TAG = "DeleteDataBaseActivity";
+    Context mContext;
+    DB db;  // ergo
+    DB2 db2;  // tread
+    SQLiteDatabase sql;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deletedb);
         mContext = this;
-        setToolbar();
+//        setToolbar();
         setStatusbar();
+        db = new DB(this);
+        db2 = new DB2(this);
 
-        Button btn_ergo = (Button) findViewById(R.id.btn_ergo);
-        btn_ergo.setOnClickListener(v -> {
+        TextView txt_ergo = (TextView) findViewById(R.id.txt_ergo);
+        txt_ergo.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
             builder.setTitle("ERGO")
                     .setMessage("저장된 내용을 전부 지울까요?")
-                    .setPositiveButton("네", (dialog, which) -> Log.d(TAG, "onClick: ergo 클리어;;;"))
+                    .setPositiveButton("네", (dialog, which) -> {
+                        Log.d(TAG, "onClick: ergo 클리어;;;");
+                        sql = db.getWritableDatabase();
+                        db.onUpgrade(sql, 1, 2);
+                    })
                     .setNegativeButton("아니오", (dialog, which) -> Log.d(TAG, "onClick: ergo 휴 다행"))
                     .show()
                     .create();
         });
-
-        Button btn_tread = (Button) findViewById(R.id.btn_tread);
-        btn_tread.setOnClickListener(v -> {
+        TextView txt_tread = (TextView) findViewById(R.id.txt_tread);
+        txt_tread.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
             builder.setTitle("TREAD")
                     .setMessage("저장된 내용을 전부 지울까요?")
-                    .setPositiveButton("네", (dialog, which) -> Log.d(TAG, "onClick: TREAD 클리어;;;"))
+                    .setPositiveButton("네", (dialog, which) -> {
+                        Log.d(TAG, "onClick: TREAD 클리어;;;");
+                        sql = db2.getWritableDatabase();
+                        db2.onUpgrade(sql, 1, 2);
+                    })
                     .setNegativeButton("아니오", (dialog, which) -> Log.d(TAG, "onClick: TREAD 휴 다행"))
                     .show()
                     .create();
         });
     }
 
-    public void setToolbar() {
-        Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(mytoolbar);
-        getSupportActionBar().setTitle("");
-        TextView bar_title = (TextView) findViewById(R.id.bar_title);
-    }
+//    public void setToolbar() {
+//        Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(mytoolbar);
+//        getSupportActionBar().setTitle("");
+//    }
 
     public void setStatusbar() {
         Window window = getWindow();
